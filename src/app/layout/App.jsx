c3bc1218182479cfbf3f12be5b2309be370c0f28
tsukaryu <drgon8otsuka@gmail.com>
2021-05-17 21:57:32 +0900

    comment out #2

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -2,9 +2,5 @@ import './style.css';
 import HomePage from '../../feature/home/HomePage';
 
 export default function App() {
-  return (
-    <div className='App'>
-      <HomePage />
-    </div>
-  );
+  return <div className='App'>{/* <HomePage /> */}</div>;
 }
