d9b282688de84be9355fc73390f8bedc0d643b0f
tsukaryu <drgon8otsuka@gmail.com>
2021-05-25 21:34:45 +0900

    refactoring tmdbReducer

--- src/app/store/rootReducer.js
+++ src/app/store/rootReducer.js
@@ -2,12 +2,10 @@ import { combineReducers } from 'redux';
 import shelfReducer from '../../feature/shelfs/shelfReducer';
 import asyncReducer from '../async/asyncReducer';
 import tmdbReducer from '../tmdb/tmdbReducer';
-import popularFilmReducer from '../../feature/popular/popularFilmReducer';
 
 const rootReducer = combineReducers({
   shelf: shelfReducer,
   async: asyncReducer,
-  popular: popularFilmReducer,
   tmdb: tmdbReducer,
 });
 
