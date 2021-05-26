fa59574fb71f1a2236ca215eb91b8b2bbf901ed8
tsukaryu <drgon8otsuka@gmail.com>
2021-05-26 14:24:06 +0900

    fix error Warning: Function components cannot be given refs.

--- src/feature/auth/LoginForm.jsx
+++ src/feature/auth/LoginForm.jsx
@@ -53,72 +53,83 @@ export default function LoginForm() {
   const classes = useStyles();
   const dispatch = useDispatch();
 
+  const Wrapper = React.forwardRef(({ children }, ref) => {
+    return (
+      <div ref={ref} tabIndex={-1} style={{ pointerEvents: 'none' }}>
+        {children}
+      </div>
+    );
+  });
+
   return (
     <ModalWrapper>
-      <Formik
-        initialValues={{
-          email: '',
-          password: '',
-        }}
-        validate={(values) => {
-          const errors = {};
-          if (!values.email) {
-            errors.email = 'Required';
-          } else if (
-            !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
-          ) {
-            errors.email = 'Invalid email address';
-          }
-          return errors;
-        }}
-        onSubmit={(values, { setSubmitting }) => {
-          setTimeout(() => {
-            setSubmitting(false);
-            alert(JSON.stringify(values, null, 2));
-          }, 500);
-        }}
-      >
-        {({ submitForm, isSubmitting }) => (
-          <Paper className={classes.paper}>
-            <Avatar className={classes.avatar}>
-              <Lock />
-            </Avatar>
-            <Typography variant='h5' className={classes.marginBottom}>
-              Sign in
-            </Typography>
-            <Form className={classes.form}>
-              <Field
-                component={TextField}
-                name='email'
-                type='email'
-                label='Email'
-                variant='outlined'
-                className={classes.marginBottom}
-              />
-              <br />
-              <Field
-                component={TextField}
-                type='password'
-                label='Password'
-                name='password'
-                variant='outlined'
-                className={classes.marginBottom}
-              />
-              {isSubmitting && <LinearProgress />}
-              <br />
-              <Button
-                variant='contained'
-                color='primary'
-                disabled={isSubmitting}
-                onClick={submitForm}
-                className={classes.button}
-              >
-                SIGN IN
-              </Button>
-            </Form>
-          </Paper>
-        )}
-      </Formik>
+      <Wrapper>
+        <Formik
+          initialValues={{
+            email: '',
+            password: '',
+          }}
+          validate={(values) => {
+            const errors = {};
+            if (!values.email) {
+              errors.email = 'Required';
+            } else if (
+              !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
+            ) {
+              errors.email = 'Invalid email address';
+            }
+            return errors;
+          }}
+          onSubmit={(values, { setSubmitting }) => {
+            setTimeout(() => {
+              console.log('submit');
+              setSubmitting(false);
+              alert(JSON.stringify(values, null, 2));
+            }, 500);
+          }}
+        >
+          {({ submitForm, isSubmitting }) => (
+            <Paper className={classes.paper}>
+              <Avatar className={classes.avatar}>
+                <Lock />
+              </Avatar>
+              <Typography variant='h5' className={classes.marginBottom}>
+                Sign in
+              </Typography>
+              <Form className={classes.form}>
+                <Field
+                  component={TextField}
+                  name='email'
+                  type='email'
+                  label='Email'
+                  variant='outlined'
+                  className={classes.marginBottom}
+                />
+                <br />
+                <Field
+                  component={TextField}
+                  type='password'
+                  label='Password'
+                  name='password'
+                  variant='outlined'
+                  className={classes.marginBottom}
+                />
+                {isSubmitting && <LinearProgress />}
+                <br />
+                <Button
+                  variant='contained'
+                  color='primary'
+                  disabled={isSubmitting}
+                  onClick={submitForm}
+                  className={classes.button}
+                >
+                  SIGN IN
+                </Button>
+              </Form>
+            </Paper>
+          )}
+        </Formik>
+      </Wrapper>
     </ModalWrapper>
   );
 }
